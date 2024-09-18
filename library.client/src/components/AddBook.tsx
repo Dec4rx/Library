03e712e207d6896c5c54e9c008eae5b0c9a7e443
Dec4rx <decarwar@gmail.com>
2024-09-17 22:08:27 -0600

    Revert "Cambio de notificaciones"
    This reverts commit f7cf116f859ba7ec0b97b0843685d3f768e6e1e6.

--- library.client/src/components/AddBook.tsx
+++ library.client/src/components/AddBook.tsx
@@ -1,50 +1,18 @@
-import React, { useState } from 'react';
 import { BookInput } from '../types/Books';
-import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { useState } from 'react';
+import Modal from './Modal';
 
 interface EditBookProps {
     handleGetBooks: () => Promise<void>;
 }
 
+
 const AddBook: React.FC<EditBookProps> = ({ handleGetBooks }) => {
-    const [book, setBook] = useState<BookInput>({
-        title: '',
-        author: '',
-        isbn: '',
-        genre: '',
-        year: 0,
-        description: '',
-        pages: 0,
-    });
+    const [book, setBook] = useState<BookInput>();
     const [errors, setErrors] = useState<any>({});
+    const [isAddModalOpen, setIsAddModalOpen] = useState<boolean>(false);
 
-    // Validación del libro
-    const validateBook = (book: BookInput) => {
-        const newErrors: any = {};
-
-        if (!book.title || book.title.length < 3) newErrors.title = 'Title must be at least 3 characters long';
-        if (!book.author || book.author.length < 3) newErrors.author = 'Author must be at least 3 characters long';
-        if (!book.isbn || book.isbn.length < 10 || book.isbn.length > 13) newErrors.isbn = 'ISBN must be between 10 and 13 characters long';
-        if (!book.genre || book.genre.length < 3) newErrors.genre = 'Genre must be at least 3 characters long';
-        if (book.year < 1 || book.year > new Date().getFullYear()) newErrors.year = 'Year must be between 1 and the current year';
-        if (!book.description || book.description.length < 10) newErrors.description = 'Description must be at least 10 characters long';
-        if (book.pages < 1) newErrors.pages = 'Pages must be a positive number';
-
-        return newErrors;
-    };
-
-    // Manejo del formulario para agregar un libro
     const handleAddBook = async (book: BookInput) => {
-        setErrors({});
-
-        // Validación del lado del cliente
-        const validationErrors = validateBook(book);
-        if (Object.keys(validationErrors).length > 0) {
-            setErrors(validationErrors);
-            return;
-        }
-
         try {
             const response = await fetch('api/book', {
                 method: 'POST',
@@ -55,149 +23,119 @@ const AddBook: React.FC<EditBookProps> = ({ handleGetBooks }) => {
             });
 
             if (!response.ok) {
-                const errorData = await response.text(); // Obtener el texto de la respuesta
-                console.error('Error response:', errorData);
-                // Manejo de errores del servidor
-                setErrors({ general: 'Failed to add book. Please try again.' });
-                throw new Error('Network response was not ok');
+                const responseData = await response.json();
+                if (responseData.errors) {
+                    setErrors(responseData.errors);
+                }
+                throw new Error(`Network response was not ok. Status: ${await response.json()}`);
             }
 
-            // Manejo de respuesta exitosa
-            await handleGetBooks(); // Refrescar la lista de libros
-            setBook({
-                title: '',
-                author: '',
-                isbn: '',
-                genre: '',
-                year: 0,
-                description: '',
-                pages: 0,
-            }); // Limpiar campos del formulario
-            toast.success('Book added successfully!'); // Mostrar mensaje de éxito
+            handleGetBooks(); // Refresh the book list
         } catch (err) {
             console.error('Failed to add book', err);
-            toast.error('Failed to add book. Please try again.'); // Mostrar mensaje de error
         }
     };
+
+
 
     return (
         <div>
             <form
                 onSubmit={(e) => {
                     e.preventDefault();
-                    handleAddBook(book);
+                    if (book) {
+                        handleAddBook(book);
+                    }
+                    if (Object.keys(errors).length > 0 ) {
+                        setIsAddModalOpen(true);
+                    }
                 }}
             >
-                <label className={errors.title ? 'error-label' : ''}>
+                <Modal isOpen={isAddModalOpen} onClose={() => setIsAddModalOpen(false)}>
+                    <div className="error-messages">
+                        {Object.entries(errors).map(([field, errorMessages]) => (
+                            <div key={field}>
+                                <strong>{field}:</strong> {(errorMessages as string[]).join(', ')}
+                            </div>
+                        ))}
+                    </div>
+                </Modal>
+                <label>
                     Title:
                     <input
                         type="text"
                         placeholder="Title"
-                        minLength={3}
-                        maxLength={50}
-                        value={book.title}
-                        onChange={(e) => setBook({ ...book, title: e.target.value })}
-                        required
+                        min={3}
+                        max={50}
+                        value={book?.title}
+                        onChange={(e) => setBook({ ...book!, title: e.target.value })}
                     />
-                    {errors.title && <p className="error">{errors.title}</p>}
                 </label>
-
-                <label className={errors.author ? 'error-label' : ''}>
+                <label>
                     Author:
                     <input
                         type="text"
                         placeholder="Author"
-                        minLength={3}
-                        maxLength={100}
-                        value={book.author}
-                        onChange={(e) => setBook({ ...book, author: e.target.value })}
-                        required
+                        min={3}
+                        max={100}
+                        value={book?.author}
+                        onChange={(e) => setBook({ ...book!, author: e.target.value })}
                     />
-                    {errors.author && <p className="error">{errors.author}</p>}
                 </label>
-
-                <label className={errors.isbn ? 'error-label' : ''}>
-                    ISBN:
+                <label>
+                    Isbn:
                     <input
                         type="text"
-                        placeholder="ISBN"
-                        minLength={10}
-                        maxLength={13}
-                        value={book.isbn}
-                        onChange={(e) => setBook({ ...book, isbn: e.target.value })}
-                        required
+                        placeholder="Isbn"
+                        min={10}
+                        max={13}
+                        value={book?.isbn}
+                        onChange={(e) => setBook({ ...book!, isbn: e.target.value })}
                     />
-                    {errors.isbn && <p className="error">{errors.isbn}</p>}
                 </label>
-
-                <label className={errors.genre ? 'error-label' : ''}>
+                <label>
                     Genre:
                     <input
                         type="text"
                         placeholder="Genre"
-                        minLength={3}
-                        maxLength={50}
-                        value={book.genre}
-                        onChange={(e) => setBook({ ...book, genre: e.target.value })}
-                        required
+                        min={3}
+                        max={50}
+                        value={book?.genre}
+                        onChange={(e) => setBook({ ...book!, genre: e.target.value })}
                     />
-                    {errors.genre && <p className="error">{errors.genre}</p>}
                 </label>
-
-                <label className={errors.year ? 'error-label' : ''}>
+                <label>
                     Year:
                     <input
                         type="number"
                         placeholder="Year"
                         min={1}
                         max={new Date().getFullYear()}
-                        value={book.year}
-                        onChange={(e) => setBook({ ...book, year: Number(e.target.value) })}
-                        required
+                        value={book?.year}
+                        onChange={(e) => setBook({ ...book!, year: Number(e.target.value) })}
                     />
-                    {errors.year && <p className="error">{errors.year}</p>}
                 </label>
-
-                <label className={errors.description ? 'error-label' : ''}>
+                <label>
                     Description:
                     <textarea
                         placeholder="Description"
                         minLength={10}
-                        value={book.description}
-                        onChange={(e) => setBook({ ...book, description: e.target.value })}
-                        required
+                        value={book?.description}
+                        onChange={(e) => setBook({ ...book!, description: e.target.value })}
                     />
-                    {errors.description && <p className="error">{errors.description}</p>}
                 </label>
-
-                <label className={errors.pages ? 'error-label' : ''}>
+                <label>
                     Pages:
                     <input
                         type="number"
                         placeholder="Pages"
-                        value={book.pages}
+                        value={book?.pages}
                         min={1}
-                        onChange={(e) => setBook({ ...book, pages: Number(e.target.value) })}
-                        required
+                        onChange={(e) => setBook({ ...book!, pages: Number(e.target.value) })}
                     />
-                    {errors.pages && <p className="error">{errors.pages}</p>}
                 </label>
-
                 <button type="submit" className="primary">Add</button>
             </form>
-
-            <ToastContainer
-                position="bottom-center"
-                autoClose={5000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                className="toast-container-custom"
-            />
         </div>
     );
 };
