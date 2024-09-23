cefc4c3b61ff85822b9ca12b08a591c6daed3097
Dec4rx <decarwar@gmail.com>
2024-09-23 17:26:40 -0600

    Refactor layout component and update navigation in App.tsx

--- library.client/src/App.tsx
+++ library.client/src/App.tsx
@@ -1,16 +1,24 @@
 import {
     BrowserRouter as Router,
     Routes,
-    Route} from "react-router-dom";
-import Layout from "./components/Layout";
+    Route
+} from "react-router-dom";
+import NavBar from "./components/Layout";
 import Home from "./components/Home";
+import imagePath from "./assets/logo.jpg";
 import './App.css';
 
 const App = () => {
+    const items = [
+        { name: "Home", path: "/home", icon: "fas fa-home" },
+        { name: "Product", path: "/product", icon: "fas fa-box" },
+        { name: "Service", path: "/service", icon: "fas fa-concierge-bell" },
+    ];
+
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Layout/> }>
+                <Route path="/" element={<NavBar brandName="Library" imageSrcPath={imagePath} navItems={items} />}>
                     <Route index element={<Home />} />
                     {/*<Route path="*" element={<NoPage />} /> //TODO Implement */}
                 </Route>
--- library.client/src/components/Layout.tsx
+++ library.client/src/components/Layout.tsx
@@ -1,27 +1,81 @@
 
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
+import React, { useState } from 'react';
+
+interface NavBarProps {
+  brandName: string;
+  imageSrcPath: string;
+  navItems: { name: string; path: string; icon: string }[]; // Actualiza el tipo
+}
 
 
-const Layout: React.FC = () => {
+const NavBar: React.FC<NavBarProps> = ({ brandName, imageSrcPath, navItems }) => {
+  const [selectedIndex, setSelectedIndex] = useState(0);
   return (
-    <div className="d-flex flex-column min-vh-100">
-      <header className="">
-        <div className="container">
-          <h1 className="display-4">Digital Library</h1>
+    <div>
+      <nav className="navbar navbar-expand-md navbar-light bg-white shadow">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            <img
+              src={imageSrcPath}
+              width="60"
+              height="60"
+              className="d-inline-block align-center"
+              alt=""
+            />
+            <span className="fw-bolder fs-4">{brandName}</span>
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-md-1">
+              {navItems.map((item, index) => (
+                <li key={item.name} className="nav-item">
+                  <Link
+                    className={
+                      selectedIndex === index
+
+                        ? "nav-link active fw-bold"
+                        : "nav-link"
+                    }
+                    to={item.path}
+                    onClick={() => setSelectedIndex(index)}
+                  >
+                    <i className={item.icon}></i> {item.name}{" "}
+                    {/* Agregar ícono */}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <form className="form-inline my-2 my-lg-0">
+              UserName &nbsp;&nbsp;
+              <img
+                className="img-thumbnail rounded-circle"
+                width={50}
+                height={50}
+                src="https://mdbootstrap.com/img/Photos/Avatars/img%20(30).jpg"
+              />
+            </form>
+          </div>
         </div>
-      </header>
+      </nav>
+
       <main className="flex-grow-1">
         <div className="container my-5">
           <Outlet />
         </div>
       </main>
-      <footer className="">
-        <div className="container">
-          <p className="mb-0">© 2024 Digital Library. All rights reserved. </p>
-        </div>
-      </footer>
     </div>
   );
 };
 
-export default Layout;
+export default NavBar;
